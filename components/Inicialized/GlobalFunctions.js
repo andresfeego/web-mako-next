7b708c5b4b64d9b1115127070c14ff60222f7e5c
julianchocmanrique <julianchocmanrique@gmail.com>
2024-03-01 23:02:06 -0500

    crear empresa

--- components/Inicialized/GlobalFunctions.js
+++ components/Inicialized/GlobalFunctions.js
@@ -6,3 +6,7 @@ export const MaysPrimera = (string) => {
 String.prototype.toProperCase = function () {
     return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
+
+export function generaCodigo(length) {
+    // Implementación de la función generaCodigo
+  }
