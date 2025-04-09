98b1f000f6149f3c3d1d9792c60be8fd11a51094
Andres Manrique <andres.feego@gmail.com>
2025-04-08 21:29:25 -0500

    Update vercel node 18

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,7 @@ const App = ({ Component, pageProps }) => {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.getRegistrations().then(registrations => {
       for (const registration of registrations) {
+        console.log(registration);
         registration.unregister();
       }
     });
